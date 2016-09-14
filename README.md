c518580021ebe72257f61a9671e16b5694743318
Victor Fragoso <victor.fragoso@mail.wvu.edu>
2016-09-14 08:48:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This project illustrates how to create a window using GLFW.
+This project is part of the CS470 - Introduction to Computer
+Graphics class at WVU.
+
+Please send comments or questions to:
+Victor Fragoso (victor.fragoso@mail.wvu.edu)
---
+++ create_glfw_window.cc
@@ -0,0 +1,135 @@
+// Copyright (C) 2016 West Virginia University.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of West Virginia University nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Please contact the author of this library if you have any questions.
+// Author: Victor Fragoso (victor.fragoso@mail.wvu.edu)
+
+#include <iostream>
+#include <string>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// Annonymous namespace for constants and helper functions.
+namespace {
+// Window dimensions.
+constexpr int kWindowWidth = 640;
+constexpr int kWindowHeight = 480;
+
+// Error callback.
+static void ErrorCallback(int error, const char* description) {
+  std::cerr << "ERROR: " << description << std::endl;
+}
+
+// Key callback.
+static void KeyCallback(GLFWwindow* window,
+                        int key,
+                        int scancode,
+                        int action,
+                        int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  }
+}
+
+void SetWindowHints() {
+  // Sets properties of windows and have to be set before creation.
+  // GLFW_CONTEXT_VERSION_{MAJOR|MINOR} sets the minimum OpenGL API version
+  // that this program will use.
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  // Sets the OpenGL profile.
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  // Sets the property of resizability of a window.
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+    // Initialize the GLFW library.
+  if (!glfwInit()) {
+    return -1;
+  }
+
+  // Setting the error callback.
+  glfwSetErrorCallback(ErrorCallback);
+
+  // Setting Window hints.
+  SetWindowHints();
+
+  // Create a window and its OpenGL context.
+  const std::string window_name = "My Window";
+  GLFWwindow* window = glfwCreateWindow(kWindowWidth,
+                                        kWindowHeight,
+                                        window_name.c_str(),
+                                        nullptr,
+                                        nullptr);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  // Make the window's context current.
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+  glfwSetKeyCallback(window, KeyCallback);
+
+  // Initialize GLEW.
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "Glew did not initialize properly!" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  // Loop until the user closes the window.
+  while (!glfwWindowShouldClose(window)) {
+    // RenderScene();
+
+    // Swap front and back buffers.
+    glfwSwapBuffers(window);
+
+    // Poll for and process events.
+    glfwPollEvents();
+  }
+
+  // Cleaning up tasks.
+  // Destroy window.
+  glfwDestroyWindow(window);
+
+  // Tear down GLFW library.
+  glfwTerminate();
+
+  return 0;
+}
+
